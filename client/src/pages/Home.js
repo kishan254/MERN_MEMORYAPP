abdc0e5418661af53e39aec7ae53140c69354b4b
Kishan Gosrani <kishan.gosrani22@gmail.com>
2022-05-29 13:02:02 +0800

    client

---
+++ client/src/pages/Home.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { Grid, Transition } from "semantic-ui-react";
+
+import { AuthContext } from "../context/auth";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../utils/graphql";
+
+function Home() {
+	const { user } = useContext(AuthContext);
+	const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+	let posts = <h1>Loading..</h1>;
+	if (!loading) {
+		// console.log(data.getPosts);
+		posts = data.getPosts.map((post) => (
+			<Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+				<PostCard post={post} />
+			</Grid.Column>
+		));
+	}
+	return (
+		<Grid columns={1}>
+			<Grid.Row className='page-title'>
+				<h1>Recent Posts</h1>
+			</Grid.Row>
+			<Grid.Row>
+				{user && (
+					<Grid.Column style={{ marginBottom: 20 }}>
+						<PostForm />
+					</Grid.Column>
+				)}
+				<Transition.Group duration={400}>{posts}</Transition.Group>
+			</Grid.Row>
+		</Grid>
+	);
+}
+
+export default Home;
