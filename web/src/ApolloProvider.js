d4cfa797b10c481590fc40c632ebf8c43fbbbfc7
Kishan Gosrani <kishan.gosrani22@gmail.com>
2022-05-30 21:53:56 +0800

    revert http link to localhost:5000

--- web/src/ApolloProvider.js
+++ web/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-	uri: "https://memorytrust-server.herokuapp.com/",
+	uri: "http://localhost:5000",
 });
 
 
